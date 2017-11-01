d1032bf4d442715ef26472333745f3a05ebb35f3
Cswl Coldwind <cswl1337@gmail.com>
2017-11-01 13:37:18 +0545

    Update patch.sh with new functions and identation

--- patch.sh
+++ patch.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 SOURCE_DIR=$(pwd)
-SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
-	read -r path tracking remote branch url <<< "$LINE"
-     cd "$SOURCE_DIR/$path"
-     printf "$path \n"
-     if [[ "$1" = "log" ]]; then
-     	git log --oneline "$tracking/$branch...$remote/$branch"
-     	printf '\n'
-     elif [[ "$1" = "checkout" ]]; then
-     	    git checkout "$remote/$branch"
-     	    printf '\n'
-     elif [[ "$1" = "rebase" ]]; then
-     	    git checkout "$remote/$branch"
-          git rebase "$tracking/$branch"
-     	    printf '\n'
-				elif [[ "$1" = "forcepush" ]]; then
-						 git checkout "$remote/$branch"
-						 git push --force "$remote" "$branch"
-						 printf '\n'
-     fi
-
+  read -r path tracking remote branch url <<<"$LINE"
+  cd "$SOURCE_DIR/$path"
+  printf "$path \n"
+  if [[ "$1" == "log" ]]; then
+    git log --oneline --first-parent "$tracking/$branch^..$remote/$branch"
+    printf '\n'
+  elif [[ "$1" == "checkout" ]]; then
+    git checkout "$remote/$branch"
+    printf '\n'
+  elif [[ "$1" == "rebase" ]]; then
+    git checkout "$remote/$branch"
+    git rebase "$tracking/$branch"
+    printf '\n'
+  elif [[ "$1" == "forcepush" ]]; then
+    git checkout "$remote/$branch"
+    git push --force "$remote" "$branch"
+    printf '\n'
+  elif [[ "$1" == "unshallow" ]]; then
+    git fetch --depth=5 "$tracking" "$branch"
+    printf '\n' 
+  fi
 
 done < "$SCRIPT_DIR/patch.list"
